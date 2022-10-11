e895305ca2e34411f9e25e605b2755f48e7319ec
ahmadfaqih796 <ahmadfaqih796@gmail.com>
2022-10-11 20:51:55 +0700

    add update & delete komik

--- controllers/komik.controller.js
+++ controllers/komik.controller.js
@@ -14,11 +14,26 @@ router.get('/:id',async function(req,res) {
   const data = await schema.SeriesSchema.findById(req.params.id);
   res.json(data);
 });
+// upload komik
 router.post('/',async function(req,res) {
   const data = req.body
   const komik = new schema.SeriesSchema(data);
   await komik.save();
   res.json({pesan: "Berhasil menyimpan data komik"})
+});
+// edit komik
+router.put('/:id',async function(req,res) {
+  // res.json({pesan: "hallo put"})
+  await schema.SeriesSchema.findByIdAndUpdate(
+    req.params.id, 
+    req.body
+    )
+    res.json({pesan: "berhasil mengedit data"})
+});
+// hapus komik
+router.delete('/:id',async function(req,res) {
+  await schema.SeriesSchema.findByIdAndRemove(req.params.id);
+  res.json({pesan: "berhasil hapus data"})
 });
 
 module.exports = router;
